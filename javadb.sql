eeca641b9c4d2d6dd2913c02bc064591a7d239bd
sunghoon <tjdgns_573@naver.com>
2023-03-28 18:11:14 +0900

    JAVADB 연습

--- javadb.sql
+++ javadb.sql
@@ -1,1 +1,67 @@
-#javadb
+-- javadb
+
+--userTBL 테이블 생성
+--no(번호-숫자(4)), username(이름-한글(4)), birthYear(년도-숫자(4)), addr(주소-문자(한글,숫자)), mobile(010-1234-1234)
+--no 는 pk 제약조건 지정(제약조건명 pk_userTBL)
+
+CREATE TABLE userTBL(
+    no NUMBER(4)CONSTRAINT pk_usertbl PRIMARY KEY,
+    username  NVARCHAR2(10) NOT NULL,
+    birthyear number(4) NOT NULL , 
+    ADDR NVARCHAR2 (50) NOT NULL,  
+    MOBILE NVARCHAR2 (20));
+
+drop table userTBL;
+
+--시퀀스 생성
+--user_seq 생성(기본)
+create sequence user_seq;
+
+-- insert
+-- no : user_seq 값 넣기
+INSERT INTO userTBL(no,username, birthYear, addr, mobile)
+VALUES(user_seq.NEXTVAL, '홍길동',2010,'서울시 종로구 123', '010-1234-5678');
+
+commit;
+
+--모든 컬럼 not null
+
+-- paytype : pay_no(숫자-(1) pk), info(문자-card, cash)
+-- paytype_seq 생성
+CREATE TABLE paytype(
+pay_no number(1) PRIMARY KEY,
+info varchar2(10) not null);
+
+CREATE SEQUENCE paytype_seq;
+
+INSERT INTO paytype values(paytype_seq.NEXTVAL, 'card');
+INSERT INTO paytype values(paytype_seq.NEXTVAL, 'cash');
+
+SELECT * FROM paytype; -- 1: card, 2 : cash
+
+--suser : user_id(숫자-(4)pk), name(문자-한글), pay_no(숫자-(1): paytype 테이블에 있는 pay_no 참조해서 사용)
+
+CREATE TABLE suser(
+user_id number(4) PRIMARY KEY,
+name varchar2(20) not null,
+pay_no number(1) not null references paytype(pay_no));
+
+--product
+--product_id(숫자-(8)pk), pname(문자), price(숫자), content(문자)
+CREATE TABLE product(
+product_id number(8) PRIMARY KEY,
+pname varchar2(30) not null,
+price number(8) not null,
+content varchar2(50));
+
+CREATE SEQUENCE product_seq;
+
+--sorder
+--order_id(숫자-(8)pk), user_id(user 테이블의 user_id참조), product_id(product 테이블의 product_id참조)
+--order_seq 생성
+CREATE TABLE sorder(
+order_id number(8) PRIMARY KEY,
+user_id number(4) not null REFERENCES suser(user_id),
+product_id number(8) not null REFERENCES product(product_id));
+
+CREATE SEQUENCE order_seq;
--- system.sql
+++ system.sql
@@ -1,9 +1,27 @@
+-- 사용자 : scott, hr => 데이터베이스 접속하여 데이터를 관리하는 계정
+--        테이블, 인덱스, 뷰 등 여러 객체가 사용자별로 생성됌
+--        업무에 따라 사용자들을 나눠서 관리
+
+
+-- 스키마 : 데이터간 관계, 데이터 구조, 제약조건 등 데이터를 관리 저장하기 위해 정의한 데이터베이스 구조의 범위
+-- 스키마 = 사용자(오라클 데이터베이스 경우)
+
+-- 사용자 생성 : 사용자 생성 권한이 필요함 ==> sys, system
+
+
+
+-- 오라클 버전 올라가면서 사용자 생성시 요구하는 접두사 생략을 위해
 ALTER SESSION SET "_ORACLE_SCRIPT" = TRUE;
 
+--사용자생성
+--비밀번호는 대소문자 구별함
 CREATE USER JAVADB IDENTIFIED BY 12345
 DEFAULT TABLESPACE USERS
 TEMPORARY TABLESPACE TEMP;
 
+ALTER USER javadb QUOTA 2M ON USERS;
+
+-- 사용자 권한 부여
 GRANT CONNECT, RESOURCE TO JAVADB;
 
 
@@ -29,3 +47,56 @@ GRANT UNLIMITED TABLESPACE TO SCOTT;
 GRANT CONNECT,RESOURCE,UNLIMITED TABLESPACE TO SCOTT IDENTIFIED BY TIGER;
 ALTER USER SCOTT DEFAULT TABLESPACE USERS;
 ALTER USER SCOTT TEMPORARY TABLESPACE TEMP;
+
+--test 사용자 생성 / 비번 12345
+CREATE USER test IDENTIFIED BY 12345;
+
+--접속권한 부여하지 않으면 안됌
+
+--권한 권리
+--1)시스템 권한
+-- 사용자 생성 / 수정 / 삭제
+-- 데이터베이스 접근
+-- 여러 객체 생성(view, synonym) 및 관리 권한
+
+--2)객체 권한
+-- 테이블 수정, 삭제, 인덱스 생성, 삽입, 참조, 조회, 수정
+-- 뷰 삭제, 삽입, 생성, 조회, 수정
+-- 시퀀스 수정, 조회
+-- 프로시저, 함수, 패키지 권한
+
+--CREATE SESSION 을 test에게 부여
+GRANT CREATE SESSION TO test;
+
+GRANT RESOURCE, CREATE TABLE TO test;
+
+--테이블 스페이스 USERS에 권한이 없습니다.
+ALTER USER test DEFAULT TABLESPACE USERS;
+ALTER USER test TEMPORARY TABLESPACE TEMP;
+ALTER USER test QUOTA 2M ON USERS; --USERS 에 공간 설정
+
+--scott에게 tesr가 생성한 member 테이블 조회권한 부여
+--GRANT SELECT ON member TO scott;
+
+--권한 취소(member에게준 SELECT, INSERT권한 가져오기
+--REVOKE SELECT, INSERT ON member FROM scott;
+
+--롤 : 여러권한들의 모임
+--CONNECT 롤 : CREATE SESSION
+--RESOURCE 롤 : CREATE TRIGGER, CREATE SEQUENCE, CREATE PROCEDURE, CREATE TABLE...
+
+
+
+--사용자삭제
+DROP USER test;
+DROP USER test CASCADE; --test와 관련된 모든 객체 같이삭제
+
+
+
+
+--사용자 생성 + 테이블 스페이스 권한부여
+CREATE USER test2 IDENTIFIED BY 12345
+DEFAULT TABLESPACE USERS
+TEMPORARY TABLESPACE TEMP;
+--권한 부여
+GRANT CONNECT, RESOURCE TO test2;
